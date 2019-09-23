f0b56f77b134b6131b4e1faa0288b5279d1b24ab
Naftuli Kay <me@naftuli.wtf>
2019-09-23 15:02:26 -0700

    done with sleeping

--- src/handlers.rs
+++ src/handlers.rs
@@ -8,15 +8,27 @@ use std::time::Duration;
 use crate::config::CliArgs;
 use tokio::prelude::FutureExt;
 
+use uuid::Uuid;
+
 pub fn handler(data: Data<CliArgs>) -> Box<dyn Future<Item = HttpResponse, Error = Error>> {
-    log::debug!("Sleeping for {:?}...", data.sleep());
     sleeper(data.sleep())
 }
 
 fn sleeper(duration: Duration) -> Box<dyn Future<Item = HttpResponse, Error = Error>> {
-    Box::new(
-        future::empty::<(), ()>()
-            .timeout(duration)
-            .then(|_r| Ok(HttpResponse::Ok().finish())),
-    )
+    let req_id = Uuid::new_v4();
+
+    log::debug!("{{request_id = {}}} Sleeping for {:?}.", req_id, duration);
+
+    Box::new(future::empty::<(), ()>().timeout(duration).then(move |_r| {
+        let pretty = format!("{:?}", duration);
+        let millis = duration.as_millis();
+
+        log::debug!("{{request_id = {}}} Sending response.", req_id);
+
+        Ok(HttpResponse::Ok()
+            .header("X-Request-Id", req_id.to_string())
+            .header("X-Sleep-Duration", pretty)
+            .header("X-Sleep-Duration-Millis", format!("{}", millis))
+            .finish())
+    }))
 }
